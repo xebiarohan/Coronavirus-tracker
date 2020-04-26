7302e0bdd18e584cf35f980ee83838095fe1de5a
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-04-26 19:22:13 +0400

    Resolving dependencies

--- src/main/java/com/practiveprojects/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/practiveprojects/coronavirustracker/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package com.practiveprojects.coronavirustracker.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String getData() {
+        return "home";
+    }
 }
