f055a4519c84b76d623778c13dfe93c1d463047a
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-05-01 21:20:43 +0400

    sorting country data

--- frontend/src/DataTableview/DataSort.js
+++ frontend/src/DataTableview/DataSort.js
@@ -1,1 +1,40 @@
-\
+import React, { useState, useMemo } from "react";
+
+const useSortableData = (items, config = null) => {
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const sortedItems = React.useMemo(() => {
+    if (items.length) {
+      let sortableItems = [...items];
+      if (sortConfig !== null) {
+        sortableItems.sort((a, b) => {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === "ascending" ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === "ascending" ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      return sortableItems;
+    }
+    return items;
+  }, [items, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return { items: sortedItems, requestSort };
+};
+
+export default useSortableData;
--- frontend/src/DataTableview/DataTableView.js
+++ frontend/src/DataTableview/DataTableView.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -8,13 +8,16 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import axios from "axios";
+import useSortableData from "./DataSort";
 
 const DataTableView = (props) => {
   const [countryWiseCount, setCountryWiseCount] = useState({
     virusData: [],
   });
 
-  const [sortedField, setSortedField] = useState();
+  const { items, requestSort } = useSortableData(countryWiseCount.virusData);
+
+
 
   useEffect(() => {
     axios.get("/trackerData").then((response) => {
@@ -49,29 +52,12 @@ const DataTableView = (props) => {
   });
 
   const classes = useStyles();
-  React.useMemo(() => {
-    if (sortedField) {
-      let currentData = [...countryWiseCount.virusData];
-      console.log(new Date());
-      currentData = currentData.sort((a, b) => {
-        if (a[sortedField] < b[sortedField]) {
-          return -1;
-        }
-        if (a[sortedField] > b[sortedField]) {
-          return 1;
-        }
-        return 0;
-      });
-      console.log(new Date());
-      setCountryWiseCount({ virusData: currentData });
-    }
-  }, [sortedField]);
 
   const headers = (
     <TableRow>
       <StyledTableCell
         onClick={() => {
-          setSortedField("country");
+          requestSort("country");
         }}
       >
         Country
@@ -90,7 +76,7 @@ const DataTableView = (props) => {
         <Table className={classes.table} aria-label="customized table">
           <TableHead>{headers}</TableHead>
           <TableBody>
-            {countryWiseCount.virusData.map((country, index) => (
+            {items.map((country, index) => (
               <StyledTableRow key={index}>
                 <StyledTableCell>{country.country}</StyledTableCell>
                 <StyledTableCell>
