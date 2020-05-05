8c797ce9dab8eb44af794353dcf272b0a32a0287
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-05-05 17:16:02 +0400

    Added Google authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,19 @@ import DataGraphView from "./containers/DataGraphView/DataGraphView";
 import TotatlCasesContext from "./context/TotalCasesContext";
 import axios from "axios";
 import { Button } from "@material-ui/core";
-import GithubLogo from './assets/githhub_logo.png';
-import linkedIn from './assets/linkedIn.png';
+import GithubLogo from "./assets/githhub_logo.png";
+import linkedIn from "./assets/linkedIn.png";
+import GoogleLogin from "react-google-login";
+import DefaultProfile from "./assets/profile_default.png";
 
 class App extends Component {
   state = {
     totalCases: 0,
     countryWiseData: [],
+    userDetails: {
+      userName: "",
+      imageUrl: null,
+    },
   };
 
   static contextType = TotatlCasesContext;
@@ -32,6 +38,52 @@ class App extends Component {
     });
   }
 
+  login =
+    this.state.userDetails.imageUrl !== null ? (
+      <div className={classes.userContainer}>
+        <div className={classes.userNameContainer}>
+          Welcome{" "}
+          {this.state.userDetails.userName !== ""
+            ? this.state.userDetails.userName
+            : "Guest"}
+        </div>
+
+        <img
+          style={{ borderRadius: "50%", paddingTop: "0.4rem" }}
+          alt="Profile pic"
+          src={
+            this.state.userDetails.imageUrl !== null
+              ? this.state.userDetails.imageUrl
+              : DefaultProfile
+          }
+          width="25"
+          height="40"
+        />
+      </div>
+    ) : (
+      <div className={classes.googleLoginContainer}>
+        <GoogleLogin
+          clientId="1057494228025-qi8pi9mq2jj06sqo0ihb46eqrlv67lde.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+    );
+
+  responseGoogle = (response) => {
+    this.setState({
+      ...this.state,
+      userDetails: {
+        userName: response.profileObj.name,
+        imageUrl: response.profileObj.imageUrl,
+      },
+    });
+
+    console.log(this.state);
+  };
+
   render() {
     console.log(this.state.totalCases + "App");
     return (
@@ -43,7 +95,12 @@ class App extends Component {
       >
         <BrowserRouter>
           <div>
-            <h2>Corona Virus Tracker Application</h2>
+            <div className={classes.header}>
+              <h2 className={classes.nameContainer}>
+                Corona Virus Tracker Application
+              </h2>
+              {this.login}
+            </div>
 
             <div className={classes.container}>
               <div className={classes.totalCount}>
@@ -77,23 +134,14 @@ class App extends Component {
                 target="_blank"
                 href="https://github.com/xebiarohan/Coronavirus-tracker"
               >
-                <img
-                  alt="Github"
-                  src={GithubLogo}
-                  width="40"
-                  height="80"
-                />
+                <img alt="Github" src={GithubLogo} width="40" height="80" />
               </a>
-              <a className={classes.linkedInLogo}
+              <a
+                className={classes.linkedInLogo}
                 target="_blank"
                 href="https://www.linkedin.com/in/rohan-aggarwal-585707145/"
               >
-                <img
-                  alt="LinkedIn"
-                  src={linkedIn}
-                  width="40"
-                  height="55"
-                />
+                <img alt="LinkedIn" src={linkedIn} width="40" height="55" />
               </a>
             </div>
           </div>
