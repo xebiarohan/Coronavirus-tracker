99dfe146134ec81aff7e5372f63fb202a46d55d4
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-05-08 23:19:33 +0400

    Aded sonarQube config

--- src/main/java/com/practiveprojects/coronavirustracker/controllers/RestApiController.java
+++ src/main/java/com/practiveprojects/coronavirustracker/controllers/RestApiController.java
@@ -4,6 +4,7 @@ package com.practiveprojects.coronavirustracker.controllers;
 import com.practiveprojects.coronavirustracker.models.LocationStats;
 import com.practiveprojects.coronavirustracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/virusData")
 public class RestApiController {
+
 
     @Autowired
     private CoronaVirusDataService coronaVirusDataService;
